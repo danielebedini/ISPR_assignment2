be81e8a2bfb18142d687ecd866b73ceb9a97ee4d
Daniele Bedini <108469620+danielebedini@users.noreply.github.com>
2024-04-11 14:27:19 +0200

    Added tr and ts set

--- try_plot.py
+++ try_plot.py
@@ -23,6 +23,8 @@ print(X_encoded.head())
 X = data.drop('class', axis=1)  # Features
 y = data['class']  # Target
 
+tr_data_unencoded = pd.concat([X, y], axis=1)
+
 # Perform one-hot encoding on features
 X_encoded = pd.get_dummies(X)
 
@@ -35,14 +37,26 @@ from pgmpy.models import BayesianModel
 # Concatenate encoded features with target variable
 data_encoded = pd.concat([X_encoded, y], axis=1)
 
+from sklearn.model_selection import train_test_split
+
+# split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42)
+
+tr_set_encoded = pd.concat([X_train, y_train], axis=1)
+
+#test_set_encoded = pd.concat([X_test, y_test], axis=1)
+
+'''
 # Initialize the PC algorithm object
-pc = PC(data_encoded)
+pc = PC(tr_set_encoded)
 
 # Run the PC algorithm to learn the structure of the Bayesian Network
 estimated_model = pc.estimate()
 
 # Get the learned structure (edges) of the Bayesian Network
 print(estimated_model.edges())
+
+
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -58,3 +72,16 @@ pos = nx.spring_layout(G)  # Set layout for better visualization
 nx.draw(G, pos, with_labels=True, node_size=1200, node_color="skyblue", font_size=12, font_weight="bold", arrows=True)
 plt.title("Learned Bayesian Network Structure (PC Algorithm)")
 plt.show()
+
+from scores import compute_correlation_score, compute_structure_score
+
+
+
+# Compute the correlation score of the learned model
+correlation_score = compute_correlation_score(estimated_model, test_set)
+print(f"Correlation Score: {correlation_score}")
+
+# Compute the structure score of the learned model
+structure_score = compute_structure_score(estimated_model, test_set)
+print(f"Structure Score: {structure_score}")
+'''
